1e438a15ddf5f3f570def8ca8a57982644081253
jefree <jgarzon920429@gmail.com>
2013-06-18 10:00:25 -0500

    implementacion funcion es_primop para que las pruebas EsPrimoTest pasen

--- factoresPrimos.py
+++ factoresPrimos.py
@@ -1,6 +1,16 @@
 import unittest
 import multiplosTest
 
+def es_primo(numero):
+
+	factores = 0
+
+	for i in range(1, int(numero**(0.5))+1):
+		if numero % i == 0:
+			factores += 1
+
+	return factores == 1
+
 class EsPrimoTest(unittest.TestCase):
 
 	def test_prueba_que_7_es_primo(self):
@@ -13,7 +23,7 @@ class EsPrimoTest(unittest.TestCase):
 		self.assertEqual(es_primo(5), True)
 
 	def test_prueba_que_4_no_es_primo(self):
-		self.assertEqual(es_primo(2), False)
+		self.assertEqual(es_primo(4), False)
 
 	def test_prueba_que_20000_no_es_primo(self):
 		self.assertEqual(es_primo(20000), False)
